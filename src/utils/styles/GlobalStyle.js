21446a81d9f6c70d30ca1adffaa43c14be68de7e
Michel Brousse <mbweb47@gmail.com>
2022-02-28 09:53:53 +0100

    💄 Add global style and 🍱 assets

--- src/utils/styles/GlobalStyle.js
+++ src/utils/styles/GlobalStyle.js
@@ -1,11 +1,57 @@
 import { createGlobalStyle } from 'styled-components'
+import colors from './colors'
 
 const GlobalStyle = createGlobalStyle`
 // globals css rules here
 * {
+    box-sizing: border-box;
+}
+
+html {
+	font-size: 1rem;
+    color: ${colors.neutral700};
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    scroll-behavior: smooth;
 }
 
 #root {
+  margin: auto;
+  max-width: 1920px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+    flex: 1;
+  }
+
+p {
+    line-height: 1.5;
+  }
+ul {
+    margin: 0;
+    padding: 0;
+  
+    list-style-type: none;
+  }
+
+  .sr-only {
+  border: 0 !important;
+  clip: rect(1px, 1px, 1px, 1px) !important; /* 1 */
+  -webkit-clip-path: inset(50%) !important;
+  clip-path: inset(50%) !important; /* 2 */
+  height: 1px !important;
+  margin: -1px !important;
+  overflow: hidden !important;
+  padding: 0 !important;
+  position: absolute !important;
+  width: 1px !important;
+  white-space: nowrap !important; /* 3 */
 }
 `
 
--- src/utils/styles/colors.js
+++ src/utils/styles/colors.js
@@ -1,6 +1,13 @@
 const colors = {
-    primary: '#020203',
-    secondary: '#FF0101',
-  }
-  
-  export default colors
+  primary500: '#00bc77',
+  primary600: '#00935d',
+  primary700: '#005834',
+  secondary900: '#12002b',
+  danger500: '#e60000',
+  neutral300: '#ccc',
+  neutral400: '#aaa',
+  neutral700: '#2c3e50',
+  neutral900: '#222',
+}
+
+export default colors
