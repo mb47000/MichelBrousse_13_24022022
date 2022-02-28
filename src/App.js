fd47ec8f650a6673824f0e9d6f91c1fe71ecd272
Michel Brousse <mbweb47@gmail.com>
2022-02-28 09:22:15 +0100

    ✨ Implement routing and create matching pages with functional component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,25 @@
-import { Fragment } from 'react'
 import GlobalStyle from './utils/styles/GlobalStyle'
-
-import { useDispatch, useSelector } from 'react-redux'
-import loginActions from './redux/features/login/actions'
-import userActions from './redux/features/user/actions'
-import { selectLogin, selectUser } from './redux/selectors'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header/'
+import Footer from './components/Footer/'
+import Home from './pages/Home/'
+import Login from './pages/Login/'
+import Profile from './pages/Profile/'
+import Error from './pages/Error/'
 
 function App() {
-  const dispatch = useDispatch()
-  const login = useSelector(selectLogin)
-  const user = useSelector(selectUser)
-
   return (
-    <Fragment>
+    <>
       <GlobalStyle />
-      <div>Hello Projet 13</div>
-      <div>Login: {login?.status}</div>
-      <div>UserFirstName: {JSON.stringify(user?.profile?.firstName)}</div>
-      <div>UserLastName: {JSON.stringify(user?.profile?.lastName)}</div>
-      <button
-        onClick={async () => {
-          await dispatch(
-            loginActions.fetchToken('tony@stark.com', 'password123')
-          )
-          dispatch(userActions.fetchProfile())
-        }}
-      >
-        Login
-      </button>
-      <button
-        onClick={() => {
-          dispatch(userActions.updateProfile('Michel', 'Brousse'))
-        }}
-      >
-        Update Michel Brousse
-      </button>
-      <button
-        onClick={() => {
-          dispatch(userActions.updateProfile('Duncan', 'Mcleod'))
-        }}
-      >
-        Update Duncan
-      </button>
-
-      <button
-        onClick={() => {
-          dispatch(loginActions.logout())
-          dispatch(userActions.logout())
-        }}
-      >
-        Logout
-      </button>
-    </Fragment>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
